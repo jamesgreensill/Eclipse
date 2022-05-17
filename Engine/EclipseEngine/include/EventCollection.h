590392afd923ffc16c05a516a8e1b6aa2a05c5eb
jwrmg <69098500+jwrmg@users.noreply.github.com>
2022-05-17 10:57:12 +1000

    created event collection
    map of events to key

---
+++ Engine/EclipseEngine/include/EventCollection.h
@@ -0,0 +1,58 @@
+#pragma once
+#include <unordered_map>
+
+#include "EclipseEvent.h"
+
+namespace Eclipse
+{
+	namespace Engine
+	{
+		template <typename TKey, typename ...TArgs>
+		class EventCollection
+		{
+			typedef EclipseEvent<TArgs...> GenericEvent;
+		public:
+			std::unordered_map<TKey, GenericEvent*> eventCollection = {};
+
+			bool AddEvent(TKey key, GenericEvent* event);
+			GenericEvent* GetEvent(TKey key);
+
+			// operator overloads
+			GenericEvent* operator[](TKey key);
+		private:
+			GenericEvent* _getEvent(TKey key);
+
+			bool _addEvent(TKey key, GenericEvent* event);
+		};
+
+		template <typename TKey, typename ... TArgs>
+		bool EventCollection<TKey, TArgs...>::AddEvent(TKey key, GenericEvent* event)
+		{
+			return _addEvent(key, event);
+		}
+
+		template <typename TKey, typename ... TArgs>
+		typename EventCollection<TKey, TArgs...>::GenericEvent* EventCollection<TKey, TArgs...>::GetEvent(TKey key)
+		{
+			return _getEvent(key);
+		}
+
+		template <typename TKey, typename ... TArgs>
+		typename EventCollection<TKey, TArgs...>::GenericEvent* EventCollection<TKey, TArgs...>::operator[](TKey key)
+		{
+			return _getEvent(key);
+		}
+
+		template <typename TKey, typename ... TArgs>
+		typename EventCollection<TKey, TArgs...>::GenericEvent* EventCollection<TKey, TArgs...>::_getEvent(TKey key)
+		{
+			return eventCollection[key];
+		}
+
+		template <typename TKey, typename ... TArgs>
+		bool EventCollection<TKey, TArgs...>::_addEvent(TKey key, GenericEvent* event)
+		{
+			return eventCollection.try_emplace(key, event).second;
+		}
+	}
+}
---
+++ Engine/EclipseEngine/src/EventCollection.cpp
@@ -0,0 +1,1 @@
+#include "EventCollection.h" 
