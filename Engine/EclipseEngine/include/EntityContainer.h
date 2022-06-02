3a75cab17b8ee3feacbdcc35d360c7bfa8ff5545
jwrmg <69098500+jwrmg@users.noreply.github.com>
2022-06-02 19:18:42 +1000

    Added Type Safety - T must be derived from TBase

--- Engine/EclipseEngine/include/EntityContainer.h
+++ Engine/EclipseEngine/include/EntityContainer.h
@@ -6,212 +6,184 @@
 template <class TBaseClass>
 struct EntityContainer
 {
-	std::unordered_map<std::type_index, TBaseClass*> m_Components{};
-	std::function<void(TBaseClass*)> m_CreationCallback = nullptr;
-	std::function<void(TBaseClass*)> m_DeletionCallback = nullptr;
+    std::unordered_map<std::type_index, TBaseClass*> m_Components{};
+    std::function<void(TBaseClass*)> m_CreationCallback = nullptr;
+    std::function<void(TBaseClass*)> m_DeletionCallback = nullptr;
 
-	/**
-	 * \brief EntityContainer constructor that takes 2 callbacks.
-	 * \param creationCallback This action will be called upon TBaseClass creation.
-	 * \param deletionCallback This action will be called upon TBaseClass deletion.
-	 */
-	EntityContainer(std::function<void(TBaseClass*)> creationCallback, std::function<void(TBaseClass*)> deletionCallback) :
-		m_CreationCallback(std::move(creationCallback)), m_DeletionCallback(std::move(deletionCallback)) {}
-	/**
-	 * \brief Adds an instance of type T if it does not already exist.
-	 * \tparam T
-	 * \return Created Instance.
-	 */
-	template<typename T>
-	T* AddComponent()
-	{
-		/*
-		 *
-		 *	TODO: Assert type is of component.
-		 *
-		 */
-		std::type_index type = typeid(T);
-		TBaseClass* instance = nullptr;
+    /**
+     * \brief EntityContainer constructor that takes 2 callbacks.
+     * \param creationCallback This action will be called upon TBaseClass creation.
+     * \param deletionCallback This action will be called upon TBaseClass deletion.
+     */
+    EntityContainer(std::function<void(TBaseClass*)> creationCallback, std::function<void(TBaseClass*)> deletionCallback) :
+        m_CreationCallback(std::move(creationCallback)), m_DeletionCallback(std::move(deletionCallback)) {}
+    /**
+     * \brief Adds an instance of type T if it does not already exist.
+     * \tparam T
+     * \return Created Instance.
+     */
+    template<typename T>
+    T* AddComponent()
+    {
+        static_assert(std::is_base_of<TBaseClass, T>::value, "T does not derive from TBaseClass.");
 
-		if ((instance = GetComponent<T>()))
-			return (T*)instance;
+        std::type_index type = typeid(T);
+        TBaseClass* instance = nullptr;
 
-		instance = new T();
-		m_CreationCallback((TBaseClass*)instance);
+        if ((instance = GetComponent<T>()))
+            return (T*)instance;
 
-		m_Components.emplace(type, instance);
-		return (T*)instance;
-	}
+        instance = new T();
+        m_CreationCallback((TBaseClass*)instance);
 
-	/**
-	 * \brief Removes a valid instance of type T.
-	 * \tparam T
-	 */
-	template<typename T>
-	void RemoveComponent()
-	{
-		/*
-		 *
-		 *	TODO: Assert type is of component.
-		 *
-		 */
-		std::type_index type = typeid(T);
-		const auto& it = m_Components.find(type);
+        m_Components.emplace(type, instance);
+        return (T*)instance;
+    }
 
-		if (it == m_Components.end())
-			return;
+    /**
+     * \brief Removes a valid instance of type T.
+     * \tparam T
+     */
+    template<typename T>
+    void RemoveComponent()
+    {
+        static_assert(std::is_base_of<TBaseClass, T>::value, "T does not derive from TBaseClass.");
 
-		m_DeletionCallback(it->second);
+        std::type_index type = typeid(T);
+        const auto& it = m_Components.find(type);
 
-		delete it->second;
-		m_Components.erase(it);
-	}
+        if (it == m_Components.end())
+            return;
 
-	/**
-	 * \brief Returns a valid instance of type T.
-	 * \tparam T
-	 * \param checkInheritedClasses
-	 * \return
-	 */
-	template<typename T>
-	T* GetComponent()
-	{
-		/*
-		*
-		*	TODO: Assert type is of component.
-		*
-		*/
-		std::type_index type = typeid(T);
+        m_DeletionCallback(it->second);
 
-		const auto& it = m_Components.find(type);
-		if (it != m_Components.end())
-			return (T*)it->second;
-		for (auto& pair : m_Components)
-		{
-			T* instance = dynamic_cast<T*>(pair.second);
-			if (instance)
-				return instance;
-		}
+        delete it->second;
+        m_Components.erase(it);
+    }
 
-		return nullptr;
-	}
+    /**
+     * \brief Returns a valid instance of type T.
+     * \tparam T
+     * \param checkInheritedClasses
+     * \return
+     */
+    template<typename T>
+    T* GetComponent()
+    {
+        static_assert(std::is_base_of<TBaseClass, T>::value, "T does not derive from TBaseClass.");
 
-	/**
-	 * \brief Returns all valid instances of type T
-	 * \tparam T
-	 * \param checkInheritedClasses
-	 * \return
-	 */
-	template<typename T>
-	std::vector<T*> GetComponents()
-	{
-		/*
-		*
-		*	TODO: Assert type is of component.
-		*
-		*/
-		std::vector<T*> components{};
+        std::type_index type = typeid(T);
 
-		for (auto& pair : m_Components)
-		{
-			T* instance = dynamic_cast<T*>(pair.second);
-			if (instance)
-				components.emplace_back(instance);
-		}
-		return components;
-	}
+        const auto& it = m_Components.find(type);
+        if (it != m_Components.end())
+            return (T*)it->second;
+        for (auto& pair : m_Components)
+        {
+            T* instance = dynamic_cast<T*>(pair.second);
+            if (instance)
+                return instance;
+        }
 
-	/**
-	 * \brief Returns the first valid instance of type T
-	 * \tparam T
-	 * \param checkInheritedClasses
-	 * \return
-	 */
-	template<typename T>
-	T* GetComponentInChildren()
-	{
-		/*
-		*
-		*	TODO: Assert type is of component.
-		*
-		*/
-		return nullptr;
-	}
+        return nullptr;
+    }
 
-	/**
-	 * \brief Returns all valid instances of type T in children and this object.
-	 * \tparam T
-	 * \param checkInheritedClasses
-	 * \return
-	 */
-	template<typename T>
-	std::vector<T*> GetComponentsInChildren()
-	{
-		/*
-		 *
-		 *	TODO: Assert type is of component.
-		 *
-		 */
-		std::vector<T*> components{};
-		GetComponentsInChildren(components);
-		return components;
-	}
+    /**
+     * \brief Returns all valid instances of type T
+     * \tparam T
+     * \param checkInheritedClasses
+     * \return
+     */
+    template<typename T>
+    std::vector<T*> GetComponents()
+    {
+        static_assert(std::is_base_of<TBaseClass, T>::value, "T does not derive from TBaseClass.");
 
-	template<typename T>
-	void GetComponentsInChildren(std::vector<T*>& output)
-	{
-		/*
-		*
-		*	TODO: Assert type is of component.
-		*
-		*/
-	}
+        std::vector<T*> components{};
 
-	/**
-	 * \brief Returns true if the Object has a component of type T
-	 * \tparam T
-	 * \param checkInheritedClasses
-	 * \return
-	 */
-	template<typename T>
-	bool HasComponent()
-	{
-		/*
-		*
-		*	TODO: Assert type is of component.
-		*
-		*/
-		std::type_index type = typeid(T);
+        for (auto& pair : m_Components)
+        {
+            T* instance = dynamic_cast<T*>(pair.second);
+            if (instance)
+                components.emplace_back(instance);
+        }
+        return components;
+    }
 
-		const auto& it = m_Components.find(type);
-		if (it != m_Components.end())
-			return true;
+    /**
+     * \brief Returns the first valid instance of type T
+     * \tparam T
+     * \param checkInheritedClasses
+     * \return
+     */
+    template<typename T>
+    T* GetComponentInChildren()
+    {
+        static_assert(std::is_base_of<TBaseClass, T>::value, "T does not derive from TBaseClass.");
+        static_assert(true, "NOT IMPLEMENTED");
+        return nullptr;
+    }
 
-		for (auto& pair : m_Components)
-		{
-			T* instance = dynamic_cast<T*>(pair.second);
-			if (instance)
-				return true;
-		}
+    /**
+     * \brief Returns all valid instances of type T in children and this object.
+     * \tparam T
+     * \param checkInheritedClasses
+     * \return
+     */
+    template<typename T>
+    std::vector<T*> GetComponentsInChildren()
+    {
+        static_assert(std::is_base_of<TBaseClass, T>::value, "T does not derive from TBaseClass.");
 
-		return false;
-	}
+        std::vector<T*> components{};
+        GetComponentsInChildren(components);
+        return components;
+    }
 
-	inline void RemoveComponent(std::type_index type)
-	{
-		/*
-			 *
-			 *	TODO: Assert type is of component.
-			 *
-			 */
-		const auto& it = m_Components.find(type);
+    template<typename T>
+    void GetComponentsInChildren(std::vector<T*>& output)
+    {
+        static_assert(std::is_base_of<TBaseClass, T>::value, "T does not derive from TBaseClass.");
+    }
 
-		if (it == m_Components.end())
-			return;
+    /**
+     * \brief Returns true if the Object has a component of type T
+     * \tparam T
+     * \param checkInheritedClasses
+     * \return
+     */
+    template<typename T>
+    bool HasComponent()
+    {
+        static_assert(std::is_base_of<TBaseClass, T>::value, "T does not derive from TBaseClass.");
 
-		m_DeletionCallback(it->second);
+        std::type_index type = typeid(T);
 
-		delete it->second;
-		m_Components.erase(it);
-	}
+        const auto& it = m_Components.find(type);
+        if (it != m_Components.end())
+            return true;
+
+        for (auto& pair : m_Components)
+        {
+            T* instance = dynamic_cast<T*>(pair.second);
+            if (instance)
+                return true;
+        }
+
+        return false;
+    }
+
+    inline void RemoveComponent(std::type_index type)
+    {
+        static_assert(std::is_base_of<TBaseClass, T>::value, "T does not derive from TBaseClass.");
+
+        const auto& it = m_Components.find(type);
+
+        if (it == m_Components.end())
+            return;
+
+        m_DeletionCallback(it->second);
+
+        delete it->second;
+        m_Components.erase(it);
+    }
 };
