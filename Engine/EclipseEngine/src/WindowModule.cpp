51e280e0ce1006d23e1d1c0f4d9ef30782cd3778
jwrmg <69098500+jwrmg@users.noreply.github.com>
2022-06-02 19:18:51 +1000

    Modified Intendation

--- Engine/EclipseEngine/src/WindowModule.cpp
+++ Engine/EclipseEngine/src/WindowModule.cpp
@@ -5,122 +5,123 @@
 
 namespace Eclipse
 {
-	namespace Engine
-	{
-		void WindowModule::Created()
-		{
-		}
+    namespace Engine
+    {
+        void WindowModule::Created()
+        {
+        }
 
-		void WindowModule::Deleted()
-		{
-		}
+        void WindowModule::Deleted()
+        {
+        }
 
-		void WindowModule::RetrieveGuiData()
-		{
-			EngineModule::RetrieveGuiData();
-		}
+        void WindowModule::RetrieveGuiData()
+        {
+            EngineModule::RetrieveGuiData();
+        }
 
-		void WindowModule::PreEngineInit()
-		{
-			EngineModule::PreEngineInit();
-		}
+        void WindowModule::PreEngineInit()
+        {
+            EngineModule::PreEngineInit();
+        }
 
-		void WindowModule::EngineInit()
-		{
-			Core::Window::InitWindow(Application::Instance->settings.windowWidth, Application::Instance->settings.windowHeight, Application::Instance->settings.windowTitle.c_str());
-		}
+        void WindowModule::EngineInit()
+        {
+            Core::Window::InitWindow(Application::Instance->settings.windowWidth, Application::Instance->settings.windowHeight, Application::Instance->settings.windowTitle.c_str());
+        }
 
-		void WindowModule::PostEngineInit()
-		{
-			EngineModule::PostEngineInit();
-		}
+        void WindowModule::PostEngineInit()
+        {
+            EngineModule::PostEngineInit();
+        }
 
-		void WindowModule::PostBoot()
-		{
-			EngineModule::PostBoot();
-		}
+        void WindowModule::PostBoot()
+        {
+            EngineModule::PostBoot();
+        }
 
-		void WindowModule::Boot()
-		{
-		}
+        void WindowModule::Boot()
+        {
+        }
 
-		void WindowModule::OnAwake()
-		{
-			EngineModule::OnAwake();
-		}
+        void WindowModule::OnAwake()
+        {
+            EngineModule::OnAwake();
+        }
 
-		void WindowModule::OnStart()
-		{
-			EngineModule::OnStart();
-		}
+        void WindowModule::OnStart()
+        {
+            EngineModule::OnStart();
+        }
 
-		void WindowModule::OnLateStart()
-		{
-			EngineModule::OnLateStart();
-		}
+        void WindowModule::OnLateStart()
+        {
+            EngineModule::OnLateStart();
+        }
 
-		void WindowModule::OnFrameBegin()
-		{
-		}
+        void WindowModule::OnFrameBegin()
+        {
+            
+        }
 
-		void WindowModule::OnFixedUpdate()
-		{
-			EngineModule::OnFixedUpdate();
-		}
+        void WindowModule::OnFixedUpdate()
+        {
+            EngineModule::OnFixedUpdate();
+        }
 
-		void WindowModule::OnUpdate()
-		{
-			EngineModule::OnUpdate();
-		}
+        void WindowModule::OnUpdate()
+        {
+            EngineModule::OnUpdate();
+        }
 
-		void WindowModule::OnLateUpdate()
-		{
-			EngineModule::OnLateUpdate();
-		}
+        void WindowModule::OnLateUpdate()
+        {
+            EngineModule::OnLateUpdate();
+        }
 
-		void WindowModule::OnPreDraw()
-		{
-			Core::Window::BeginDrawing();
-		}
+        void WindowModule::OnPreDraw()
+        {
+            Core::Window::BeginDrawing();
+        }
 
-		void WindowModule::OnDraw()
-		{
-			EngineModule::OnDraw();
-		}
+        void WindowModule::OnDraw()
+        {
+            EngineModule::OnDraw();
+        }
 
-		void WindowModule::OnPostDraw()
-		{
-			Core::Window::EndDrawing();
-		}
+        void WindowModule::OnPostDraw()
+        {
+            Core::Window::EndDrawing();
+        }
 
-		void WindowModule::OnGizmos()
-		{
-			EngineModule::OnGizmos();
-		}
+        void WindowModule::OnGizmos()
+        {
+            EngineModule::OnGizmos();
+        }
 
-		void WindowModule::OnPreGui()
-		{
-			EngineModule::OnPreGui();
-		}
+        void WindowModule::OnPreGui()
+        {
+            EngineModule::OnPreGui();
+        }
 
-		void WindowModule::OnGui()
-		{
-			EngineModule::OnGui();
-		}
+        void WindowModule::OnGui()
+        {
+            EngineModule::OnGui();
+        }
 
-		void WindowModule::OnPostGui()
-		{
-			EngineModule::OnPostGui();
-		}
+        void WindowModule::OnPostGui()
+        {
+            EngineModule::OnPostGui();
+        }
 
-		void WindowModule::OnFrameEnd()
-		{
-			EngineModule::OnFrameEnd();
-		}
+        void WindowModule::OnFrameEnd()
+        {
+            EngineModule::OnFrameEnd();
+        }
 
-		void WindowModule::OnDispose()
-		{
-			Core::Window::CloseWindow();
-		}
-	}
+        void WindowModule::OnDispose()
+        {
+            Core::Window::CloseWindow();
+        }
+    }
 }
